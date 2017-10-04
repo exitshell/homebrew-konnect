98d7318d0adb73531dabb99cc59adbb1af4e79ad
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-03 23:34:14 -0400

    konnect version v0.0.3

--- konnect.rb
+++ konnect.rb
@@ -1,9 +1,9 @@
 class Konnect < Formula
   desc "Konnect - connect to thing!"
   homepage "https://github.com/exitshell/konnect"
-  url "https://github.com/exitshell/konnect/releases/download/v0.0.2/konnect_0.0.2_macOS_64-bit.tar.gz"
-  version "0.0.2"
-  sha256 "72c2c404821b810189591864a5f661253b5b3cb2e68c926c53d3a8b7a6820ec0"
+  url "https://github.com/exitshell/konnect/releases/download/v0.0.3/konnect_0.0.3_macOS_64-bit.tar.gz"
+  version "0.0.3"
+  sha256 "52feedef38139002d55b1e7214dae59b5282d3354111463d72a05d61dc31f610"
 
   def install
     bin.install "konnect"
